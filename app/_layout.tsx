1d9b3b205da067ff10f3d6f5ac8fcd1baefeebfb
Salvador Vasquez <salvador97vasqz@gmail.com>
2025-08-24 15:00:06 -0600

    Create React Native app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from "react";
+import { Text, View } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import "../global.css";
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View>
+      <Text className="text-[20px] text-blue-400">RootLayout</Text>
+    </View>
   );
-}
+};
+
+export default RootLayout;
